07c90ba2386f1dc6554f2715b7104443fab41207
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-08-15 14:40:39 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 const { resolve } = require("path");
 
 let app = new Vue ({
-    el: '#info',
+    el: '.app',
     data: {
         count: 0,
         way: [],
         cur: 1,
         roundStarted:false,
-        roundWay: []
+        roundWay: [],
+        index: 0,
+        isOver: false,
+        startBtn: document.querySelector('#start')
+
     },
     methods:{
         start(){
-            this.roundStarted = true;
+            if(this.isOver) {
+                this.startBtn.removeAttribute("disabled");
+                this.roundStarted = false;
+                return;
+            }
             this.count++;
             do{
                 this.way.push(Math.ceil(0.5 + Math.random() * 3));
                 this.way.forEach((element, i) => {
                     setTimeout(function(){
                         document.querySelector(`.btn${element}`).style.opacity = 1;
-                        setTimeout(() => {document.querySelector(`.btn${element}`).style.opacity = .6;}, 500)
-                    }, 1000 * ++i)
+                        setTimeout(() => {document.querySelector(`.btn${element}`).style.opacity = .6;}, 500);
+                        if(i == this.way.length){
+                            this.roundStarted = true;
+                            console.log(this.roundStarted);
+                        } 
+                    }.bind(this), 1000 * ++i)
                     
                 });
                 
                 this.cur = this.count+1;
             }while(this.cur < this.count)
             event.target.disabled = true;
+            this.startBtn = event.target;
             this.roundWay = this.way;
         },
         round(event){
-            console.log(1);
             const target = event.target;
-            console.log(target);
+            this.isOver = false;
             if(this.roundStarted){
-                console.log(target);
+                    if(target.classList.contains(`btn${this.way[this.index]}`)){
+                        console.log(this.index);
+                        this.index++;
+                        if(this.index < this.way.length){
+                            return
+                        }
+                    } else{
+                        this.startBtn.removeAttribute("disabled");
+                        this.isOver = true;
+                        this.index = 0;
+                        this.cur = 1;
+                        this.way = [];
+                        this.count = 0;
+                        this.roundWay = [];
+                        alert('GAMEOVER');
+                    }
+                if(this.index === this.way.length && !this.isOver){
+                    this.roundStarted = false;
+                    this.start();
+                    this.index = 0;
+                } 
             }
         }
         
